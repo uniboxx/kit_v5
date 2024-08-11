6c144a9eea0078b197cf79755f6003db05290eeb
uniboxx <unibox@duck.com>
2024-08-11 23:00:09 +0200

    aggiunta progress bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,9 +5,19 @@
 <h1>Home Page on /</h1>
 <section id="counter">
   <button onclick="{() => counter--}">➖</button>
-  <p>Count {counter}</p>
+  {#if counter < 10}
+    <p>Count {counter}</p>
+  {:else}
+    <p>Finish</p>
+  {/if}
   <button onclick="{() => counter++}">➕</button>
 </section>
+{#if counter < 10}
+  <progress type="progress" value="{counter}" max="10"
+    >{counter * 10}%</progress>
+{:else}
+  <button id="reset" onclick="{(counter = 0)}">Reset</button>
+{/if}
 
 <style lang="scss">
   #counter {
@@ -22,5 +32,16 @@
     & p {
       text-align: center;
     }
+  }
+  progress {
+    display: block;
+  }
+
+  #reset {
+    padding: 0.5rem 1rem;
+    font-size: 1.5rem;
+    background-color: lightgreen;
+    border-radius: 10px;
+    color: #333;
   }
 </style>
