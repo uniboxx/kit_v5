b07a06c205dd113a618410097720a2395a37e1d1
uniboxx <unibox@duck.com>
2024-08-11 20:00:10 +0200

    fixed links

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -5,10 +5,10 @@
 <h1>Titolo della Pagina</h1>
 <nav>
   <ul>
-    <li><a href="/kit_v5">Home</a></li>
-    <li><a href="/about">About</a></li>
-    <li><a href="/contact">Contact</a></li>
-    <li><a href="/more">More</a></li>
+    <li><a href="/kit_v5/">Home</a></li>
+    <li><a href="/kit_v5/about">About</a></li>
+    <li><a href="/kit_v5/contact">Contact</a></li>
+    <li><a href="/kit_v5/more">More</a></li>
   </ul>
 </nav>
 
