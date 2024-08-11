70bb759cdc58a403e74ca566270833e4ee833f86
uniboxx <unibox@duck.com>
2024-08-11 20:46:41 +0200

    remove doubled kit in config

--- svelte.config.js
+++ svelte.config.js
@@ -6,13 +6,12 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    kit: {
-      adapter: adapter({
-        fallback: '404.html',
-      }),
-      paths: {
-        base: process.env.NODE_ENV === 'production' ? '/kit_v5' : '',
-      },
+
+    adapter: adapter({
+      fallback: '404.html',
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/kit_v5' : '',
     },
   },
 };
